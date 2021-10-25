56f8215717389a531d803bda1d028ee3a6fea119
JakRys <jakubrysavy00@gmail.com>
2021-10-25 10:10:59 +0000

    Improve doc strings

--- seqrep/labeling.py
+++ seqrep/labeling.py
@@ -15,17 +15,21 @@ class Labeler(abc.ABC, BaseEstimator, TransformerMixin, Picklable):
 
     @abc.abstractmethod
     def transform(self, X, y=None):
-        raise NotImplemented()
-
-    @abc.abstractmethod
-    def inverse_transform(self, X, y=None):
-        raise NotImplemented()
+        raise NotImplementedError
 
 
 class NextColorLabeler(Labeler):
     """
     NextColorLabeler applies binary labeling (0 or 1) based on the next candle 
     if it is bullish or bearish.
+
+    Parameters
+    ----------
+    column_start: str
+        Column name of the first considered value of the future candle.
+    
+    column_end: str
+        Column name of the second considered value of the future candle.
     """
 
     def __init__(self, column_start='open', column_end='close'):
@@ -33,27 +37,23 @@ class NextColorLabeler(Labeler):
         self.column_end = column_end
 
     def transform(self, X, y=None): 
-        # labels = (X[self.column_end] - X[self.column_start]).shift(-1) > 0
-        # ic(type(X))
         if not isinstance(X, pd.DataFrame):
             X = pd.DataFrame(X, columns=["open", "high", "low", "close", "volume"])
-        labels = ((X[self.column_end] - X[self.column_start]).shift(-1) > 0).astype(int)
-        # return labels.iloc[:-1]
+        labels = ((X[self.column_end].shift(-1) - X[self.column_start]).shift(-1) > 0).astype(int)
         return labels.fillna(0)
-
-    def inverse_transform(self, X, y=None):
-        return X
 
 
 class RegressionLabeler(Labeler):
+    """
+    Find the maximum and minimum value change during selected future steps.
+    
+    Parameters
+    ----------
+    duration: int
+        Maximal length for reaching label value
+    """
 
     def __init__(self, duration: int = 1):
-        """
-        Parameters
-        ----------
-        duration: int
-            Maximal length for reaching label value
-        """
         self.duration = duration
 
     def transform(self, X, y=None): 
@@ -65,43 +65,36 @@ class RegressionLabeler(Labeler):
             labels.loc[labels.index[i], ('negative_label',)] = X["close"][i:i + self.duration -1].min()
         labels['positive_label'] -= X['close']          # to do or not ?
         labels['negative_label'] -= X['close']          # to do or not ?
-        # return [positive_labels, negative_labels]
         return labels
-
-    def inverse_transform(self, X, y=None):
-        return X
 
 
 class ClassificationLabeler(Labeler):
     """
     ClassificationLabeler applies ternary labeling according to future values.
+    Parameters
+    ----------
+    duration: int
+        Maximal length for reaching label value,
+        i.e number of future datapoints
+
+    pip_size: float
+        Size of one pip in units of currency (usually 0.0001)
+
+    target_profit: float
+        Size of the target profit in pips
+    
+    stop_loss: float
+        Size of the stop loss in pips
     """
 
     def __init__(self, duration: int = 1, target_profit: float = 20, stop_loss: float = 15,
                  pip_size: float = 0.0001):
-        """
-        Parameters
-        ----------
-        duration: int
-            Maximal length for reaching label value,
-            i.e number of future datapoints
-
-        pip_size: float
-            Size of one pip in units of currency (usually 0.0001)
-
-        target_profit: float
-            Size of the target profit in pips
-        
-        stop_loss: float
-            Size of the stop loss in pips
-        """
         self.duration = duration
         self.pip_size = pip_size
         self.target_profit = target_profit
         self.stop_loss = stop_loss
 
     def transform(self, X, y=None): 
-        # labels = pd.DataFrame(index=X.index)
         labels = RegressionLabeler(duration=self.duration).transform(X)
         labels['label'] = 0
         labels['label'] = labels['label'].mask(
@@ -110,8 +103,4 @@ class ClassificationLabeler(Labeler):
         labels['label'] = labels['label'].mask(
             (-labels['negative_label'] >= self.target_profit * self.pip_size) &
             (labels['positive_label'] <= self.stop_loss * self.pip_size), 2)
-        # return labels['label']
         return labels.drop(columns=['positive_label', 'negative_label'])
-
-    def inverse_transform(self, X, y=None):
-        return X
