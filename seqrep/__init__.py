2f4905a97994ab11e5f8e8e2674bbf6ced9ed3f7
JakRys <jakubrysavy00@gmail.com>
2021-12-01 09:56:48 +0000

    Add doc string

--- seqrep/__init__.py
+++ seqrep/__init__.py
@@ -1,1 +1,22 @@
+"""
+SeqRep - Scientific framework for representation in sequential data.
 
+This package aims to simplify the workflow of **evaluation of machine learning
+models**.
+It is primarily focused on sequential data. It helps with:
+
+- labeling data,
+- splitting data,
+- **feature extraction**,
+- feature reduction (i.e. selection or transformation),
+- running pipeline,
+- evaluation of results.
+
+It also allows you to **visualize** each step.
+
+The framework is designed for easy customization and extension of its
+functionality.
+
+See the GitHub project page for more details:
+https://github.com/MIR-MU/seqrep.
+"""
